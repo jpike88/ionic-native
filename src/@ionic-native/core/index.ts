6317085f84e50277c2f70f82ec813883d84f529d
Josh Pike <pikatore@gmail.com>
2018-05-08 23:54:31 +0700

    Update index.ts

--- src/@ionic-native/core/index.ts
+++ src/@ionic-native/core/index.ts
@@ -11,4 +11,8 @@ export * from './decorators/cordova-property';
 export * from './decorators/instance-property';
 export * from './decorators/interfaces';
 
-checkReady();
+if(!!window.disableIonic){
+  // do nothing, this is a web application
+} else {
+  checkReady(); 
+}
